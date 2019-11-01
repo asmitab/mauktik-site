aa6e321887c056aed2335e28deef3eee7089e0fe
Asmita Barve-Karandikar <asmitak83@gmail.com>
2019-11-01 09:24:31 -0700

    Preview of gallery

--- admin/preview-templates/section.js
+++ admin/preview-templates/section.js
@@ -6,28 +6,48 @@ const html = htm.bind(h);
 const Page = createClass({
   render() {
     const entry = this.props.entry;
+    if (entry.getIn(["data", "gallery"])) {
+      const images = entry.getIn(["data", "gallery"]);
+      let imgHtml = ``;
 
-    if (entry.getIn(["data", "imgurl"])) {
+      for (img of images) {
+        imgHtml += `<input type="checkbox" class="faux-ui-facia">
+        <div class="slide">
+          <img src="${img}" alt="Gallery Image">`;
+      }
+
       return html`
-      <div class="flex flex-2">
-        <div class="col col1">
-          <div class="image ${entry.getIn(["data", "imgshape"])} fit">
-            <a href="${entry.getIn(["data", "imghref"])}" class="link" target="_blank">
-              <img src="${entry.getIn(["data", "imgurl"])}" alt=""/></a>
-        </div>
-      </div>
-      <div class="col col2">
-        <h3>${entry.getIn(["data", "heading"])}</h3>
-        ${this.props.widgetFor("body")}
-      </div>
-    </div>`;
+      <section class="wrapper">
+        <div class="carousel">
+        ${imgHtml}
+        </div >
+      </div >
+      </section >
+      `;
+
     } else {
-      return html`
-      <main>
-      <h1>${entry.getIn(["data", "heading"], null)}</h1>
+      if (entry.getIn(["data", "imgurl"])) {
+        return html`
+  < div class="flex flex-2" >
+    <div class="col col1">
+      <div class="image ${entry.getIn([" data", "imgshape"])} fit">
+            <a href="${entry.getIn([" data", "imghref"])}" class="link" target="_blank">
+              <img src="${entry.getIn([" data", "imgurl"])}" alt=""/></a>
+        </div >
+      </div >
+  <div class="col col2">
+    <h3>${entry.getIn(["data", "heading"])}</h3>
+    ${this.props.widgetFor("body")}
+  </div>
+    </div > `;
+      } else {
+        return html`
+  < main >
+  <h1>${entry.getIn(["data", "heading"], null)}</h1>
 
-      ${this.props.widgetFor("body")}
-    </main>`;
+${ this.props.widgetFor("body")}
+    </main > `;
+      }
     }
   }
 });
